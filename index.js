f9a53191f9e1f92441837f690686cb48f7d14d45
tasrif sirazee <tasrifsirazee@outlook.com>
2024-05-30 02:20:57 -0400

    final

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5300;
+
+// middleware
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://tr-tourism.web.app"], // Remove trailing slashes
   })
 );
 app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@test.mqwonpn.mongodb.net/?retryWrites=true&w=majority&appName=test`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,12 +23,15 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
     // await client.connect();
     const spotCollection = client.db("coffeeDB").collection("spot");
+    const userCollection = client.db("coffeeDB").collection("user");
     const countryCollection = client.db("coffeeDB").collection("country");
+
     app.get("/spot", async (req, res) => {
       try {
         const cursor = spotCollection.find();
@@ -44,6 +51,7 @@ async function run() {
         res.status(500).send({ error: "Failed to add spot" });
       }
     });
+
     app.get("/spot/:id", async (req, res) => {
       try {
         const id = req.params.id;
@@ -79,6 +87,7 @@ async function run() {
         res.status(500).send({ error: "Failed to update spot" });
       }
     });
+
     app.delete("/spot/:id", async (req, res) => {
       try {
         const id = req.params.id;
@@ -134,6 +143,9 @@ async function run() {
         res.status(500).send({ error: "Failed to fetch user's list" });
       }
     });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -142,8 +154,8 @@ async function run() {
     // await client.close();
   }
 }
-// done
 run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Tr tourism server ");
 });
