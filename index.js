99914927974b164afe305c152e484cb8d6615063
tasrif sirazee <tasrifsirazee@outlook.com>
2024-05-30 00:31:22 -0400

    sixsth

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 5300;
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@test.mqwonpn.mongodb.net/?retryWrites=true&w=majority&appName=test`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 app.get("/", (req, res) => {
   res.send("Tr tourism server ");
 });
